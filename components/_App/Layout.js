10ac194a572808dc2717b1a6721bf1d43761a1dd
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-18 14:56:26 -0400

    readded semantic ui with media print and oload this media all

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -3,18 +3,22 @@ import Header from "./Header";
 import Aux from './Aux'
 import Topheader from './Topheader'
 import Link from 'next/link'
+ 
 function Layout({ children }) {
+  
 return (
 <>
   <Head>
- 
   
- 
-<link
-          rel="stylesheet" media="print" onLoad="this.media='all'"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
-        />
+  <meta name="viewport" content="width=device-width, initial-scale=1"></meta>
 
+<link
+rel="stylesheet" media="print" onLoad="this.media"
+href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
+/>
+
+ 
+<noscript><link rel="stylesheet" href="style.css"></link></noscript>
 <title>Hoopscript</title>
         </Head>
         <Topheader />
@@ -22,8 +26,8 @@ return (
         <Aux>
             {children}
         </Aux>
+
  
-    
 </>)
 }
 
