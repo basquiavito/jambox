04406c52869911d5fb84a84fa665d87463357c66
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-18 17:04:56 -0400

    trying async css in profileSummary

--- components/Profile/ProfileSummary.js
+++ components/Profile/ProfileSummary.js
@@ -272,7 +272,7 @@ if (this.state.showDoc){
 <Head>
  
  <link
-rel="stylesheet" media="print" onload= "this.media='all' "
+rel="stylesheet" 
 href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 />
  
