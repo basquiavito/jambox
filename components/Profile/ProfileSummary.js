33cf9aebb84521af2949bb0ea6703b9d405483d2
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-18 20:09:13 -0400

    delete twitter and instagram to check speed insight

--- components/Profile/ProfileSummary.js
+++ components/Profile/ProfileSummary.js
@@ -6,9 +6,6 @@ import Label from '../../components/units/label'
 import Title from '../../components/units/title'
 import Dekk from '../../components/units/dekk'
 import Ptraits from '../../components/units/ptraits'
-import FromDaGram from '../../components/units/fromDaGram'
- 
-import Twitter from '../../components/units/twitter'
 import Video from '../../components/units/video'
  
 import Mixtape  from '../units/mixtape'
@@ -16,263 +13,13 @@ import Play from '../units/play'
  
 import Authordate from '../units/authordate'
 import Head from 'next/head'
-class ProfileSummary extends Component{
-state={
-showHighschool:false,
-showCollege:false,
-showOffHigh: false,
-showRookie: false,
-showCareerHigh: false,
-showOffHigh: false,
-showDefHigh:false,
-showDoc:false
 
-
-}
-
-togglehighschoolHandler = () => {const doesShow = this.state.showHighschool
-this.setState({showHighschool: !doesShow})}
-
-toggleCollegeHandler = () => {const doesShow = this.state.showCollege
-this.setState({showCollege: !doesShow})}
-
-toggleRookieHandler = () => {const doesShow = this.state.showRookie
- this.setState({showRookie: !doesShow})}
-
- toggleCareerHighHandler = () => {const doesShow = this.state.showCareerHigh
-   this.setState({showCareerHigh: !doesShow})}
- 
-   toggleOffHighHandler = () => {const doesShow = this.state.showOffHigh
-     this.setState({showOffHigh: !doesShow})}
-     
-     toggleDefHighHandler = () => {const doesShow = this.state.showDefHigh
-       this.setState({showDefHigh: !doesShow})}
-
-       toggleDocHandler = () => {const doesShow = this.state.showDoc 
-         this.setState({showDoc: !doesShow})}
-
-
-
-render(){
-
-
- 
-let highschool = null;
-
-if (this.state.showHighschool){
-highschool = (
- <div className="hsContainer">
-   <div className="pageGrid">
-<div className="gridCenter">
-<Sectiontitle title="Highschool"/>
-<Label label="The Beginnings"/>
- <Video videoId={this.props.highSchool} />
- <Title title={this.props.highSchoolTitle}/>
- <Dekk dekk={this.props.highSchoolDekk}/>
- <Authordate  author={this.props.highSchoolAuthor} date={this.props.highSchoolDate} /><br/>
-
- <Label label="Growth" />
- <Video videoId={this.props.highSchool1} />
- <Title title={this.props.highSchool1Title}/>
- <Dekk dekk={this.props.highSchool1Dekk}/>
- <Authordate  author={this.props.highSchool1Author} date={this.props.highSchool1Date} /><br/>
-
-
-</div></div></div>)
-
-
-
-}
-
-let college= null;
-if (this.state.showCollege){
-college = (
-<div className="pageGrid">
- <div className="gridCenter">
- <Sectiontitle title="College"/>
- <Label label={this.props.from} />
- <Video videoId={this.props.college} />
- <Title title={this.props.collegeTitle}/>
- <Dekk dekk={this.props.collegeDekk}/>
- <Authordate  author={this.props.collegeAuthor} date={this.props.collegeDate} />
-
-</div>
-
-<div>
-<Label label={this.props.from} />
-<Video videoId={this.props.college1} />
-<Title title={this.props.college1Title}/>
-<Dekk dekk={this.props.college1Dekk}/>
-<Authordate  author={this.props.college1Author} date={this.props.college1Date} />
-
-</div>
-</div>
-)
-
-
-}
-
-let rookie= null;
-if (this.state.showRookie){
-rookie = (
- <div>
- <div>
- <Sectiontitle title="Rookie"/>
- <Label label={`Rookie ${this.props.lastname}`} />
- <Video videoId={this.props.rookieOff} />
- <Title title={this.props.rookieOffTitle}/>
- <Dekk dekk={this.props.rookieOffDekk}/>
- <Authordate  author={this.props.rookieOffAuthor} date={this.props.rookieOffDate} />
-</div>
-
-<div>
- <Label label="First seasons"/>
- <Video videoId={this.props.rookieOff1} />
- <Title title={this.props.rookieOff1Title}/>
- <Dekk dekk={this.props.rookieOff1Dekk}/>
- <Authordate  author={this.props.rookieOff1Author} date={this.props.rookieOffDate} />
-</div><br/>
-</div>
-
-)
-
-
-}
-
-let careerHigh= null;
-if (this.state.showCareerHigh){
-careerHigh = (
- <div>
- <div>
- <Sectiontitle title="Career High"/>
- <Label label="Career High"/>
- <Video videoId={this.props.careerHigh} />
- <Title title={this.props.careerHighTitle}/>
- <Dekk dekk={this.props.careerHighDekk}/>
- <Authordate  author={this.props.careerHighAuthor} date={this.props.careerHighDate} />
-</div>
-
-<div>
-<Label label="Career High"/>
- <Video videoId={this.props.careerHigh1} />
- <Title title={this.props.careerHigh1Title}/>
- <Dekk dekk={this.props.careerHigh1Dekk}/>
- <Authordate  author={this.props.careerHigh1Author} date={this.props.careerHigh1Date} />
-</div><br/>
-</div>
-)
-
-
-}
-
-let offHigh= null;
-if (this.state.showOffHigh){
-offHigh = (
- <div>
- <div>
- <Sectiontitle title="Offense Mixtape"/>
-   <Label label="Scoring" />
- <Video videoId={this.props.offHigh} />
- <Title title={this.props.offHighTitle}/>
- <Dekk dekk={this.props.offHigh1Dekk}/>
- <Authordate  author={this.props.offHighAuthor} date={this.props.offHighDate} />
-</div>
- <div>
- <Label label="Scoring II" />
- <Video videoId={this.props.offHigh1} />
- <Title title={this.props.offHigh1Title}/>
- <Dekk dekk={this.props.offHigh1Dekk}/>
- <Authordate  author={this.props.offHigh1Author} date={this.props.offHigh1Date} />
-
- <div>
- <Label label="Passing" />
- <Video videoId={this.props.assistOff} />
- <Title title={this.props.assistOffTitle} />
- <Dekk dekk={this.props.assistOffDekk}/>
- <Authordate  author={this.props.assistOffAuthor} date={this.props.assistOffDate} />
-</div>
-
-<div>
-<Label label="Signature Move" />
- <Video videoId={this.props.signatureMove} />
- <Title title={this.props.signatureMoveTitle}/>
- <Dekk dekk={this.props.signatureMoveDekk}/>
- <Authordate author={` ${this.props.signatureMoveAuthor}`} date={this.props.signatureMoveDate}/>
-</div><br/>
-</div>
-</div>
-
-)
-
-
-}
-
-
-let defHigh= null;
-if (this.state.showDefHigh){
- defHigh = (
-   <div>
- <div>
- <Sectiontitle title="Defense"/>
- <Label label="Defense"/>
- <Video videoId={this.props.defHigh} />
- <Title title={this.props.defHighTitle} />
- <Dekk dekk={this.props.defHighDekk}/>
- <Authordate  author={this.props.defHighAuthor} date={this.props.defHighDate} />
-</div>
-<div>
-<Label label="Lock Down"/>
- <Video videoId={this.props.defHigh1} />
- <Title title={this.props.defHigh1Title}/>
- <Dekk dekk={this.props.defHigh1Dekk}/>
- <Authordate  author={this.props.defHigh1Author} date={this.props.defHigh1Date} />
-</div><br/>
-</div>
-)
-
-
-}
-
-let doc= null;
-if (this.state.showDoc){
- doc = (
-   <div>
- <div>
-   
- <Sectiontitle title="Documentary"/>
- <Label label="Documentary"/>
- <Video videoId={this.props.doc} />
- <Title title={this.props.docTitle}/>
- <Dekk dekk={this.props.docDekk}/>
- <Authordate  author={this.props.docAuthor} date={this.props.docDate} />
-</div>
-
-<div>
-<Label label={`On ${this.props.lastname}`}/>
- <Video videoId={this.props.doc1} />
- <Title title={this.props.doc1Title}/>
- <Dekk dekk={this.props.doc1Dekk}/>
- <Authordate  author={this.props.doc1Author} date={this.props.doc1Date} />
-</div>
-
-
-
-
-
-
-</div>
-
-
-)
-
-
-}
-   return(
-     <>
+export default function ProfileSummary({name, lastname, accolades, accoladeAuthor, accoladeLink, accoladeSource, team, number, position, feet, inches, meters, kg, lbs, years, born, from, debut, latest1, latest1Title, latest2, latest2Title, latest3, latest3Title, latest4, latest4Title, latest5, latest5Title, latest6, latest6Title, statsId, highschool, college, rookie, offHigh, doc, defHigh, videoId, videoIdTitle, careerHigh}){
+ return<>
 <Head>
  
  <link
-rel="stylesheet" 
+rel="stylesheet" media="print" onload="this.media='all'"
 href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 />
  
@@ -285,17 +32,17 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 <section>
 <div className="pageGrid">
 <div className="gridCenter">
- <div><h1><Sectiontitle title={`${this.props.name} ${this.props.lastname}`}/></h1></div>
- <div><h1><Sectionsubtitle subtitle={`Get to know ${this.props.lastname}`}/></h1></div><br/>
+ <div><h1><Sectiontitle title={`${name} ${lastname}`}/></h1></div>
+ <div><h2><Sectionsubtitle subtitle={`Get to know ${lastname}`}/></h2></div><br/>
 
 <div>
 <figure className="quote">
 <blockquote>
-<p className="accoladeCaption"> {this.props.accolades}</p>
+<p className="accoladeCaption"> {accolades}</p>
 <figcaption>
-<Authordate author={this.props.accoladeAuthor}/> 
-<a href={this.props.accoladeLink}>
-<cite>{this.props.accoladeSource}</cite>
+<Authordate author={accoladeAuthor}/> 
+<a href={accoladeLink}>
+<cite>{accoladeSource}</cite>
 </a>
 </figcaption>
 </blockquote>
@@ -303,14 +50,14 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 </div><br/>
 
 <Label label = "Profile" />
-<Dekk dekk={` ${this.props.team} | #${this.props.number}  | ${this.props.position}`}/>
+<Dekk dekk={` ${team} | #${number}  | ${position}`}/>
 </div></div>
 </section><br/>
 
 <section className="ptraitsContainer">
 <div className="pageGrid">
 <div className="gridCenter">
-<Ptraits feet={this.props.feet} inches={this.props.inches} meters={this.props.meters} kg={this.props.kg} lbs={this.props.lbs} born={this.props.born} from={this.props.from} debut={this.props.debut} years={this.props.years}  />
+<Ptraits feet={feet} inches={inches} meters={meters} kg={kg} lbs={lbs} born={born} from={from} debut={debut} years={years}  />
 </div></div>
 </section>
 
@@ -320,23 +67,23 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 <div className="pageGrid">
 <div className="gridCenter">
   <Mixtape 
-   latest1={this.props.latest1}
-   latest1Title={this.props.latest1Title}
+   latest1={latest1}
+   latest1Title={latest1Title}
 
-   latest2={this.props.latest2}
-   latest2Title={this.props.latest2Title}
+   latest2={latest2}
+   latest2Title={latest2Title}
 
-   latest3={this.props.latest3}
-   latest3Title={this.props.latest3Title}
+   latest3={latest3}
+   latest3Title={latest3Title}
 
-   latest4={this.props.latest4}
-   latest4Title={this.props.latest4Title}
+   latest4={latest4}
+   latest4Title={latest4Title}
 
-   latest5={this.props.latest5}
-   latest5Title={this.props.latest5Title}
+   latest5={latest5}
+   latest5Title={latest5Title}
 
-   latest6={this.props.latest6}
-   latest6Title={this.props.latest6Title}
+   latest6={latest6}
+   latest6Title={latest6Title}
 
    />
   
@@ -351,7 +98,7 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 <div id="stats">
 <div className="pageGrid">
 <div className="gridCenter">
-<a href={`https://stats.nba.com/player/${this.props.statsId}/`}>
+<a href={`https://stats.nba.com/player/${statsId}/`}>
 <div className="statContainer">	 
 <Label  label="numbers" />	 
 <Sectiontitle  title="NBA Advanced Stats" /><br/> 
@@ -367,19 +114,16 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 <div className="gridCenter">
 
 
-<h3>Popular {this.props.lastname} Plays</h3>
+<h3>Popular {lastname} Plays</h3>
 <div  id="highschool">
  
  <Play 
  image=""
  title="Highschool" 
  name="The Genesis" >
- <button 
-
- className="squareButton" 
- onClick={this.togglehighschoolHandler}>See</button></Play>
+</Play>
 <section className="hsContainer">
-{highschool}
+
 </section>
 </div>
 
@@ -388,11 +132,8 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 
 <div>
  <Play image="" title="College" name="Growth" >
-   <button
-  
-    className="squareButton" 
-    onClick={this.toggleCollegeHandler}>See</button></Play>
-    {college}</div>
+</Play>
+</div>
 
 
 
@@ -400,8 +141,8 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
       <Sectiontitle title="Draft Day"/><br/>
     
       
-      <Video videoId={this.props.videoId}/>
-      <Title title={this.props.videoIdTitle}/>
+      <Video videoId={videoId}/>
+      <Title title={ videoIdTitle}/>
       
       
       
@@ -409,55 +150,43 @@ href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
 
 <div>
  <Play image="" title="Rookie Season" name="First Years" >
- <button 
- className="squareButton" 
- onClick={this.toggleRookieHandler}>See</button></Play>
-{rookie}
+ </Play>
+
 </div>
 
 
 
 <div>
  <Play image="" title="Career High" name="High Performances" >
- <button 
- className="squareButton" 
- onClick={this.toggleCareerHighHandler}>See</button></Play>
-{careerHigh}
+ </Play>
+
 </div>
 
 
 
 
-<div>
- <Play image="" title="Offense Mixtape" name="Signature Moves" >
- <button 
- className="squareButton" 
- onClick={this.toggleOffHighHandler}>See</button></Play>
-{offHigh}
+
+
 </div>
 
 <div>
  <Play image="" title="Defense Mixtape" name="LockDown" >
- <button 
- className="squareButton" 
- onClick={this.toggleDefHighHandler}>See</button></Play>
-{defHigh}
+ </Play>
+
 </div>
 
 
 <div>
  <Play image="" title="Documentary" name="Stories" >
- <button 
- className="squareButton" 
- onClick={this.toggleDocHandler}>See</button></Play>
-{doc}
+ </Play>
+
 </div><br/>
 
 
 
 
 
- </div></div>
+ </div> 
 
 
 </div>
@@ -638,11 +367,10 @@ margin-bottom: 3rem;
  </>
 
 
-   )}}
-   
+ 
+}
 
 
 
 
-
-export default ProfileSummary
+ 
