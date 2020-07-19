0a8cb9e2dd8b2d01e69156b798889b5c73d40e76
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-19 11:17:38 -0400

    with no videos---test

--- components/Profile/HighschoolSummary.js
+++ components/Profile/HighschoolSummary.js
@@ -8,133 +8,35 @@ import Dekk from '../../components/units/dekk'
 import Ptraits from '../../components/units/ptraits'
 import Mixtape  from '../units/mixtape'
 import Play from '../units/play'
-import Youtubehooks from '../../components/units/youtubehooks'
+import Youtubehooks from '../units/youtubehooks'
 import Authordate from '../units/authordate'
 import Head from 'next/head'
+import Video from '../units/video'
 
-export default function ProfileSummary({docTitle, defHighTitle, careerHighTitle, highSchool,highSchoolTitle, doc1, defHigh1, assistOffTitle, signatureMove, signatureMove1, offHigh1,assistOff, name, lastname, accolades, accoladeAuthor, accoladeLink, accoladeSource, team, number, position, feet, inches, meters, kg, lbs, years, born, from, debut, latest1, latest1Title, latest2, latest2Title, latest3, latest3Title, latest4, latest4Title, latest5, latest5Title, latest6, latest6Title, statsId, highschool, college, college1,collegeTitle, rookie, offHigh, doc, defHigh, videoId, videoIdTitle, rookieOff, rookieOffTitle,careerHigh,offHighTitle}){
+
+export default function HighschoolSummary({    highSchool, highSchoolTitle}){
  return<>
-<Head>
- 
- <link
-rel="stylesheet" media="print" onload="this.media='all'"
-href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
-/>
- 
-</Head>
+
 <div id="profile">
 <div className="pageGrid">
 <div className="gridCenter">
-
-
-<section>
-<div className="pageGrid">
-<div className="gridCenter">
- <div><h1><Sectiontitle title={`${name} ${lastname}`}/></h1></div>
- <div><h2><Sectionsubtitle subtitle={`Get to know ${lastname}`}/></h2></div><br/>
-
-<div>
-  
-<figure className="quote">
-<blockquote>
-<p className="accoladeCaption"> {accolades}</p>
-<figcaption>
-<Authordate author={accoladeAuthor}/> 
-<a href={accoladeLink}>
-<cite>{accoladeSource}</cite>
-</a>
-</figcaption>
-</blockquote>
-</figure>
-</div><br/>
-
-<Label label = "Profile" />
-<Dekk dekk={` ${team} | #${number}  | ${position}`}/>
-</div></div>
-</section><br/>
-
-<section className="ptraitsContainer">
+<Youtubehooks videoIdA={highSchool} />
+<Title title="Melo es la guasacaka"/>
  
-<Ptraits feet={feet} inches={inches} meters={meters} kg={kg} lbs={lbs} born={born} from={from} debut={debut} years={years}  />
+</div></div></div>
  
-</section>
 
 
-<div>
+ 
   
  
-  <Mixtape 
-   latest1={latest1}
-   latest1Title={latest1Title}
-
-   latest2={latest2}
-   latest2Title={latest2Title}
-
-   latest3={latest3}
-   latest3Title={latest3Title}
-
-   latest4={latest4}
-   latest4Title={latest4Title}
-
-   latest5={latest5}
-   latest5Title={latest5Title}
-
-   latest6={latest6}
-   latest6Title={latest6Title}
-
-   />
-  
-  
-  
- 
-  </div>
-
-
-
-
-<div id="stats">
- 
-<a href={`https://stats.nba.com/player/${statsId}/`}>
-<div className="statContainer">	 
-<Label  label="numbers" />	 
-<Sectiontitle  title="NBA Advanced Stats" /><br/> 
-<div className="statLabel"> 
-<span className="statsPointer"> points | assists | reb | steals... </span></div>
-</div></a></div> 
-
-
-
-
-<div id=" ">
-<div className="pageGrid">
-<div className="gridCenter">
-
-
-<h3>Popular {lastname} Plays</h3>
-
-
 
  
 
 
 
-
-
-
-</div>
-
  
 
- 
-
-
-
-
-
- </div> 
-
-
-</div>
 
 <style jsx>
     {`
@@ -308,7 +210,7 @@ margin-bottom: 3rem;
 
 
 
- </div></div></div>
+
  </>
 
 
