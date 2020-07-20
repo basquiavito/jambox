f4e253493faa9e6c4981fcd9d5443d42e05b9f01
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-20 17:44:27 -0400

    did not work webp because it doesnt works on safari

--- components/units/charts.js
+++ components/units/charts.js
@@ -11,14 +11,14 @@ import Sectionsubtitle from './sectionsubtitle'
         <Sectiontitle title="Charts" />
         <Sectionsubtitle subtitle = "Top Hoopcodes" />
 <Link href=""><a href="http://localhost:3000/hoopcode?_id=5eb5ea710f258495bf4845ca">
-<Coderow rank="1" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="adCollege.webp"/></a></Link>
+<Coderow rank="1" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/dearBasketball.jpeg"/></a></Link>
 
 
 
-<Link href=""><a><Coderow rank="2" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="lsDoc.webp"/></a></Link>
-<Link href=""><a><Coderow rank="3" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="lsDefHigh.webp"/></a></Link>
-<Link href=""><a><Coderow rank="4" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="adRookieOff.webp"/></a></Link>
-<Link href=""><a><Coderow rank="5" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$"  image="ljcollege.webp"/></a></Link>
+<Link href=""><a><Coderow rank="2" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/jordanShaq.jpg"/></a></Link>
+<Link href=""><a><Coderow rank="3" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/kobeShaq.jpg"/></a></Link>
+<Link href=""><a><Coderow rank="4" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/luka.jpeg"/></a></Link>
+<Link href=""><a><Coderow rank="5" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/aimovemj.jpg"/></a></Link>
 
 <style jsx>
 
