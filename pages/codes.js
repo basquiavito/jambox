d345603723fc321b12e4161d01c9e3839c04d181
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-20 21:07:10 -0400

    started working on codes page

--- pages/codes.js
+++ pages/codes.js
@@ -1,9 +1,13 @@
 import Youtubehooks from '../components/units/youtubehooks'
+import LazyLoad from 'react-lazy-load';
+
 
 export default function CodeVideos(){
     return<>
 <div>
+    <LazyLoad>
     <Youtubehooks videoIdA='C977RCbuqFk' videoIdB='gaMMnn1o5gA'/>
+    </LazyLoad>
 </div>
     </>
 
