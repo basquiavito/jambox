d9fd6e9222f5f26a290de30b3e24069ffee2f401
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-20 20:59:35 -0400

    started working on codes page

--- components/units/charts.js
+++ components/units/charts.js
@@ -9,12 +9,8 @@ import Sectionsubtitle from './sectionsubtitle'
 
 
 
-<Link href=""><a href="http://localhost:3000/hoopcode?_id=5eb5ea710f258495bf4845ca">
-<Coderow rank="1" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/dearBasketball.jpeg"/></a></Link>
-
-
-
-<Link href=""><a><Coderow rank="2" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/jordanShaq.jpg"/></a></Link>
+<Link href="/codes"><a><Coderow rank="1" codeTitle="Iverson cross Jordan | Daniels" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/aimovemj.jpg"/></a></Link>
+<Link href="/codes"><a><Coderow rank="2" codeTitle="Iverson cross Jordan  " hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/jordanShaq.jpg"/></a></Link>
 <Link href=""><a><Coderow rank="3" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/kobeShaq.jpg"/></a></Link>
 <Link href=""><a><Coderow rank="4" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/luka.jpeg"/></a></Link>
 <Link href=""><a><Coderow rank="5" codeTitle="Iverson cross Melo" hoopcodes="013 013 189 FG$" image="https://s3.amazonaws.com/pedales.net/aimovemj.jpg"/></a></Link>
---
+++ pages/codes.js
@@ -0,0 +1,10 @@
+import Youtubehooks from '../components/units/youtubehooks'
+
+export default function CodeVideos(){
+    return<>
+<div>
+    <Youtubehooks videoIdA='C977RCbuqFk' videoIdB='gaMMnn1o5gA'/>
+</div>
+    </>
+
+}
