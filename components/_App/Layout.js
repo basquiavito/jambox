a328c6ececdf51e750ce5e29cd30dcfde90cfdda
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-22 14:33:34 -0400

    lightweight hacker news header

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,15 +9,16 @@ return (
 <>
   <Head>
   <meta charset="utf-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1"/> 
+<meta name="viewport" content="width=device-width, initial-scale = 1.0, 
+maximum-scale=1.0, user-scalable=no"/>
   <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
   
   <link rel="stylesheet" href="/static/index.css"/>
  
 <title>Basketball's News, Plays, Profiles & Articles | Hoopscript</title>
         </Head>
-     <Topheader/>
-     <Header/>
+ <Topheader/>
+   
         <Aux>
             {children}
         </Aux>
--- components/_App/Topheader.js
+++ components/_App/Topheader.js
@@ -1,152 +1,94 @@
 import Link from 'next/link'
+const tableStyle = {padding: '2px'}
+const tdStyle = {width:'18px', paddingRight: '4px'}
+const tdAImg = { width:'18px', height: '18px', border: '1px solid white'}
+const td2ndStyle = { lineHeight: '12pt', height: '10px'}
+const td3rdStyle = {textALign: 'right', paddingRight: '4px'}
+
 
 export default function Topheader(){
     return<>
-    <div className="topHeader">
+<table id="hsmain" border="0" cellpadding="0" cellspacing="0" width="85%" bgcolor="#f6f6ef">
+<tbody><tr><td bgcolor="#ff6600">
+<table border="0" cellpadding="0" cellspacing="0" width="100%" style={tableStyle} >
+<tbody><tr>
+<td style={tdStyle}>
+<a href="https://hoopscript.vercel.app">
+<img style = {tdAImg} src="/basketball.svg" width="18" height="18" alt="H"/></a></td>
+<td style={td2ndStyle}>
+<span className="pagetop">
+<b className="hsname"><Link href="/"><a >Hoopscript</a></Link></b>
+<Link href="/profiles/profiles"><a>profiles</a></Link> | <Link href="/blogs"><a href="front">articles</a></Link> | <a href="newcomments">comments</a> | <a href="ask">ask</a> | <a href="show">show</a> |  <a href="jobs">jobs</a> | <a href="submit">submit</a> 
+</span></td>
+<td style={td3rdStyle}><span className="pagetop">
+<a href="login?goto=news">login</a>
+</span></td>
+</tr></tbody></table></td></tr></tbody></table>
 
-<div className="topBar">
-<div className="wrap">
-<div id="headerLogo">
-<a href="/" id="headerLogo">
-<img src=""/>
-</a>
-<h1> <Link href="/"><a id="headerPageTitle" >HOOPSCRIPT.COM</a></Link></h1>
-                            </div>
-                            
-              <div id="topBarLinks">              
-                            
-                            <ul>
-<li><a id="top-bar-shop" href="https://shop.ballislife.com/?utm_source=ballislife.com&amp;utm_medium=button&amp;utm_campaign=website-desktop-header" target="_blank">
- Shop
-</a>
-</li>
-<li>
-<a id="top-bar-podcasts" href="/podcasts">Podcasts </a>
-</li></ul>
-</div>
-</div>  
-</div>  
- <div id="sticky-header-sticky-wrapper" class="sticky-wrapper"  ><div id="sticky-header" class="clearfix" >
-<div class="wrap">
-<div class="main-header clearfix">
 
- 
-<div id="site-branding">
-<div id="site-detail">
-<div id="site-title">
-<a href="https://ballislife.com/" title="Hoopscript.com" rel="home"> Hoopscript.com </a>
-</div>
- 
- 
-</div>
-</div>  
- 
-
-</div> 
-</div>  
-</div></div> 
-
-</div>
 
 <style jsx>
-{`
-.topHeader {
-    text-align: center;
-    background-position: center center;
-    background-size: cover;
-    background-color: rgb(18 18 18);
-
+  {`
+  @media only screen and (max-width: 750px) and (min-width: 300px){
+  #hsmain {
+      width: 100%;
+  }}
+  tbody {
+    display: table-row-group;
+    vertical-align: middle;
+    border-color: inherit;
 }
-@media only screen and (max-width: 480px){
-#topBarLinks{
-    display: block !important;
-    float: right;
-    margin-right: 5px;
-    position: absolute;
-    top: 7px;
-    width: 158px;
-    height: 52px;
-    right: 0px
-    font-size: 20px;
-    position: relative !important;
-    top: -11px !important;
+tr {
+    display: table-row;
+    vertical-align: inherit;
+    border-color: inherit;
+}
+td {
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 10pt;
+    color: #828282;
+}
+  
+@media only screen and (max-width: 750px) and (min-width: 300px){
+td {
+    height: inherit !important;
 }}
-#topBarLinks ul li {
-    margin-left: 12px;
-    float: right;
-    list-style: none !important;
+.tableStyle {
+    pading: 2px;
 }
-@media only screen and (max-width: 480px){
-#topBarLinks ul li:before {
-    font-size: 19px
-    content: '|';
-    color: #fff;
-    vertical-align: top;
-}}
-    @media only screen and (max-width: 767px){
-    .topBar {
-        padding-left: 10px;
-        padding-top: 1px;
-    }}
-    @media only screen and (max-width: 980px){
-    .topBar {
-        padding-left: 10px;
-    }}
-    @media only screen and (max-width: 1023px){
-    .topBar {
-        padding-left: 10px;
-    }}
-    @media only screen and (max-width: 1300px){
-    .topBar {
-        padding-left: 10px;
-    }}
-.wrap{
-    max-width: 1340px;
-    margin: 0 auto;
- 
-    max-width: 98%;
-    padding: 0;
-    position: relative
-}
- 
-@media only screen and (max-width: 1300px){
-    .wrap{
-        max-width: 94%;
-    }
-}
-
-#headerLogo a{
-    display: block;
-    float: left;
-    font-size: 41px;
-    letter-spacing: -3px;
-    color: #fff;
-}
-a {
-     
-    cursor: pointer;
+a:link {
+    color: #000000;
     text-decoration: none;
-    transition-property: color;
-    transition-duration: 0.2s;
-    transition-timing-function: linear;
+    cursor: pointer;
 }
-@media only screen and (max-width: 480px){
-#headerLogo h1 {
-    position: relative !important;
-    top: 8px !important;
+@media only screen and (max-width: 750px) and (min-width: 300px){
+span.pagetop {
     display: block;
-    font-size: 28px;
+    margin: 10px 5px;
+    font-size: 12px;
 }}
-ol, ul {
-    list-style: none;
-    margin: 0;
+.pagetop {
+    font-family: Verdana, Geneva, sans-serif;
+    font-size: 10pt;
+    color: #222222;
 }
 
-`}
+@media only screen and (max-width: 750px) and (min-width: 300px){
+span.pagetop b {
+    display: block;
+    font-size: 15px;
+}}
+.hsname {
+    margin-right: 5px;
+}
+ 
+b {
+    font-weight: bold;
+}
+.pagetop a:visited {
+    color: #000000;
+}
+  `}  
 </style>
-
-
-
-
     </>
 }
