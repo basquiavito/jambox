e0dd9465dfd1ea7a5bc6478b548f5990df5e6a23
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-07-25 17:34:21 -0400

    added hyvor talk

--- pages/obb/Hesitation.js
+++ pages/obb/Hesitation.js
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import HyvorTalk from 'hyvor-talk-react'
+ 
 const hesi = () => {
  
     return <>
@@ -7,27 +7,7 @@ const hesi = () => {
     
     hesitation
 
-    <div>
-        <h1>Article Title</h1>
- 
-        <div className="comment-count-view">
- 
-          { /* Comment Counts */ }
-          <HyvorTalk.CommentCount 
-           websiteId='1313'
-           id='1313'
-          />
- 
-        </div>
-        
-        <content>Your Article Here</content>
- 
-        { /* Load Comments now */ }
-        <HyvorTalk.Embed 
-          websiteId='1313'
-          id='1313'
-        />
-      </div>
+    
 
     </>
 
