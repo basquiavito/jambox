721b28d5102df40b42f0394e43c8f044fb761282
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-08-02 19:12:23 -0400

    testing mongodb speed

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 import axios from 'axios'
 import ProfileList from '../components/Index/ProfileList'
 import baseUrl from '../utils/baseUrl'
-import ArticleList from '../components/index/ArticleList'
+import ArticleList from '../components/Index/ArticleList'
 function Homepage({ articles }) {
     
  return <ArticleList articles={articles} />
