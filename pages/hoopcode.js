9b780abf9a32ae7b09d8acd84fee579829da6cc4
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-08-03 12:56:19 -0400

    working on hoopcodes

--- pages/hoopcode.js
+++ pages/hoopcode.js
@@ -16,7 +16,7 @@ function Hoopcode({ hoopcodes }) {
 }
 
 Hoopcode.getInitialProps = async ({ query: { _id } }) => {
-const url = `${baseUrl}/api/hoopcode`;
+const url = `https://hoopscript.vercel.app/api/hoopcode`;
 const payload = { params: { _id }}
 const response = await axios.get(url, payload)
 return {hoopcodes: response.data}
