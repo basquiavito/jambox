517db70e1a0775b022208f982b9175e8508b8498
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-08-03 09:55:57 -0400

    added to hoopscript

---
+++ pages/article.js
@@ -0,0 +1,26 @@
+import axios from 'axios'
+import ArticleSummary from '../components/Article/ArticleSummary'
+import baseUrl from '../utils/baseUrl'
+ 
+
+
+function Article({ articles }) {
+ 
+  return (
+  
+  <>
+  <ArticleSummary {...articles}/>
+
+  </>
+  )
+}
+
+Article.getInitialProps = async ({ query: { _id } }) => {
+const url = `${baseUrl}/api/article`;
+const payload = { params: { _id }}
+const response = await axios.get(url, payload)
+return {articles: response.data}
+ 
+}
+
+export default Article;
---
+++ pages/articles.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import axios from 'axios'
+import ArticleList from '../components/Index/ArticleList'
+import baseUrl from '../utils/baseUrl'
+
+function Articles({ articles }) {
+ return <>
+ <ArticleList articles={articles} />
+ </>
+ 
+}
+Articles.getInitialProps = async () => {
+const url = `${baseUrl}/api/articles`;
+const response = await axios.get(url);
+return { articles : response.data }
+}
+ 
+
+ 
+export default Articles;
