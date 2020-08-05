ec314856da46159334a1b8865d649adb4ecf66a2
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-08-05 16:35:43 -0400

    changed all vercel.app to .com

--- pages/update.js
+++ pages/update.js
@@ -16,7 +16,7 @@ function Update({ updates }) {
 }
 
 Update.getInitialProps = async ({ query: { _id } }) => {
-const url = `https://hoopscript.vercel.app/api/update`;
+const url = `https://hoopscript.com/api/update`;
 const payload = { params: { _id }}
 const response = await axios.get(url, payload)
 return {updates: response.data}
