bcff57a8d0c701da6af62abb81e908dedc3675c1
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-12-31 08:14:25 -0500

    first part of pagination ---testing

--- pages/index.js
+++ pages/index.js
@@ -13,22 +13,13 @@ function Articles({ articles, totalPages  }) {
  
  <ArticlePagination totalPages={totalPages}  />
  
-
- <style jsx>
-     {`
-
- 
-    
-     
-     `}
- </style>
  </>
  
 }
 Articles.getInitialProps = async ctx  => {
 const page = ctx.query.page ? ctx.query.page : "1"
 const size="2"
-const url = 'http://localhost:3000/api/articles';
+const url = 'https://hoopscript.com/api/articles';
 const payload = { params: { page, size } }
 const response = await axios.get(url, payload);
 return response.data
