e55c30d39b041111b6a97b6e95476054c0a2fb9f
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2021-01-14 18:45:07 -0500

    nexy image to see klk

--- components/_App/Layout.js
+++ components/_App/Layout.js
@@ -9,13 +9,7 @@ function Layout({ children }) {
 return (
 <>
  <Head>
-  <link  
-          rel="stylesheet"
-          href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.2/semantic.min.css"
-        
-    
-        />  
-        
+
  </Head>
         
     <nav>
