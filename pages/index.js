0b272cdef607d3a88eb27386147c46249956e17c
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2021-01-15 12:28:49 -0500

    algolia

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,116 @@
-import App from '../components/app'
-import React from 'react';
+import algoliasearch from 'algoliasearch/lite';
+import React, { Component } from 'react';
+import Image from 'next/image'
+import {
+  InstantSearch,
+  Hits,
+  SearchBox,
+  Pagination,
+  Highlight,
+  ClearRefinements,
+  RefinementList,
+  Configure,
+} from 'react-instantsearch-dom';
+import PropTypes from 'prop-types';
+import Link from 'next/link'
+const searchClient = algoliasearch(
+  'JJQ6W5JZEV',
+  '5fa378bc2862d01482c0b8537c171acc'
+);
 
-
-function home(){
-  return <>
- 
-   
-<App />
-      
-  </>
-  
+const aisInstantSearch = {
+  maxWidth: '960px',
+  overflow: 'hidden',
+  margin: '0 auto'
 }
 
-export default home
+const leftPanel = {
+ 
+  width: '250px'
+}
+
+const rightPanel = {
+ maxWidth: '500px'
+}
+
+
+const hitName=  {
+  marginBottom: '0.5em'
+}
+
+
+const hitDescription = {
+  color: '#888',
+    fontSize: '14px',
+    marginBottom: '0.5em'
+}
+
+const hitPrice  = {
+
+}
+class App extends Component {
+  render() {
+    return (
+      <div style={aisInstantSearch} >
+        <InstantSearch indexName="profiles" searchClient={searchClient}>
+          <div style={leftPanel} >
+            <ClearRefinements />
+            <h2>Players</h2>
+            <RefinementList attribute="player"  />
+            <Configure hitsPerPage={8} />
+          </div>
+      
+          <div style={rightPanel}>
+            <SearchBox />
+            <br/>
+            <Hits hitComponent={Hit} />
+            <br/>
+            <Pagination />
+          </div>
+        </InstantSearch>
+      </div>
+    );
+  }
+}
+
+function Hit(props) {
+  return <>
+  <head>
+  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/instantsearch.css@7/themes/algolia-min.css"
+    />
+    <link rel="stylesheet" href="./static/algolia.css" />
+    <link rel="stylesheet" href="./static/index.css" />
+  </head>
+    <div>
+      <Link href={props.hit.link}><a>
+    <div className="imageContainer" >
+<picture className="">
+<Image
+src={props.hit.mediaUrl}
+alt={`A photo of ${props.hit.player}`}
+width="100px"
+height="100px"
+ 
+className="imagen"
+/></picture></div>
+      <div style={hitName} >
+        <Highlight attribute="name" hit={props.hit} />
+      </div>
+      <div style={ hitDescription } >
+        <Highlight attribute="player" hit={props.hit} />
+      </div>
+      <div className="team">{props.hit.team}</div>
+      </a></Link>
+    </div>
+    
+    
+  </>;
+}
+
+Hit.propTypes = {
+  hit: PropTypes.object.isRequired,
+};
+
+export default App;
