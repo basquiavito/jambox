8b43eec9ec6333ff60b5a814fa1aed4dc16b8e5b
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2021-01-18 11:34:32 -0500

    commiting to finish the codeRow

--- components/units/sectiontitle.js
+++ components/units/sectiontitle.js
@@ -15,7 +15,8 @@ return(
     justify-content: center;
     grid-template-columns: [grid-start] minmax(36px, 1fr) [center-start] minmax(24px, 100px) 20px minmax(24px, 100px) 20px minmax(24px, 100px) 20px minmax(24px, 100px) [center-end] minmax(36px, 1fr) [grid-end];
     grid-column: grid-start / grid-end; 
-   
+    text-align: center;
+    margin-bottom: 2rem;
  
  
 }
@@ -28,7 +29,8 @@ return(
     text-align: center;
     letter-spacing: 2px;
     grid-column: grid-start / grid-end;
-    margin-bottom: 8px;
+ 
+
 }
 
 
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import KickerHub from '../components/units/kickerHub'
 import Billboard64 from '../components/units/billboard64'
 import Sectiontitle from '../components/units/sectiontitle'
 import Coderow from '../components/units/coderow'
-
+import Sectionsubtitle from '../components/units/sectionsubtitle'
 function home(){
   return <>
  <main>
@@ -13,9 +13,8 @@ function home(){
 <KickerHub />
 </div>
 
-<Sectiontitle title="Chart"></Sectiontitle>
-
-<a href=""><Coderow rank ="1" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755593/Kenny-Anderson_fci4zv.jpg" codeTitle="Kenny Anders" hoopcodes="232" details="Chibbs " /></a>
+<Sectiontitle title="Chart"></Sectiontitle> 
+ <a href=""><Coderow rank ="1" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755593/Kenny-Anderson_fci4zv.jpg" codeTitle="Kenny Anders" hoopcodes="232" details="Chibbs " /></a>
 <Coderow rank ="2" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610669340/AIcrossMJ_bmteuq.jpg" codeTitle="Allen Iverson" hoopcodes="013" details="Smoke " />
 <Coderow rank ="3" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610756357/kd_a3nisa.jpg" codeTitle="Kevin Durant" hoopcodes="802" details="Pop back " />
 <Coderow rank ="4" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610926935/lamelonew_vzo7ge.jpg" codeTitle="Lamelo Ball" hoopcodes="804 " details="Pop Reverse" />
