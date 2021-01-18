d77e2bbde1b9022e8e6ccd76352b91da15f49e63
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2021-01-18 12:01:47 -0500

    commiting to finish the codeRow

--- components/units/codeHub.js
+++ components/units/codeHub.js
@@ -1,10 +1,94 @@
+import React, {useState} from 'react'
 import Coderow from './coderow'
+import Sectiontitle from './sectiontitle'
+
+function coderowHub() {
+    const [loadmore, setLoadMore ] = useState({
+load: false
+    })
 
 
-const coderowHub  = () => {
+
+   const loadMoreHandler = () => {
+const doesShow = loadmore.load ;
+setLoadMore({load : !doesShow})
+
+   }
     return <>
 
-<Coderow />
+<Sectiontitle title="Chart 22"></Sectiontitle>
+<a href=""><Coderow rank ="1" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755593/Kenny-Anderson_fci4zv.jpg" codeTitle="Kenny Anders" hoopcodes="232" details="Chibbs " /></a>
+<Coderow rank ="2" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610669340/AIcrossMJ_bmteuq.jpg" codeTitle="Allen Iverson" hoopcodes="013" details="Smoke " />
+<Coderow rank ="3" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610756357/kd_a3nisa.jpg" codeTitle="Kevin Durant" hoopcodes="802" details="Pop back " />
+<Coderow rank ="4" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610926935/lamelonew_vzo7ge.jpg" codeTitle="Lamelo Ball" hoopcodes="804 " details="Pop Reverse" />
+ 
+<Coderow rank ="5" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610757282/currymss_lhtmdk.jpg" codeTitle="Stephen Curry" hoopcodes="318" details="Lousiana " />
+<Coderow rank ="6" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755951/harden_atkfyy.jpg" codeTitle="James Harden" hoopcodes="518" details="Albania " />
+ 
+<Coderow rank ="7" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755940/herro_bi4pxr.jpg" codeTitle="Tyler Herro" hoopcodes="PO6" details="Rep " />
+<Coderow rank ="8" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755879/tatum_zof8gi.jpg" codeTitle="jayson Tatum" hoopcodes="80P" details="TAFA " />
+
+
+{ loadmore.load  ? <div>
+ <a href=""><Coderow rank ="9" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755593/Kenny-Anderson_fci4zv.jpg" codeTitle="Kenny Anders" hoopcodes="232" details="Chibbs " /></a>
+<Coderow rank ="10" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610669340/AIcrossMJ_bmteuq.jpg" codeTitle="Allen Iverson" hoopcodes="013" details="Smoke " />
+<Coderow rank ="11" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610756357/kd_a3nisa.jpg" codeTitle="Kevin Durant" hoopcodes="802" details="Pop back " />
+<Coderow rank ="12" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610926935/lamelonew_vzo7ge.jpg" codeTitle="Lamelo Ball" hoopcodes="804 " details="Pop Reverse" />
+ 
+<Coderow rank ="13" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610757282/currymss_lhtmdk.jpg" codeTitle="Stephen Curry" hoopcodes="318" details="Lousiana " />
+<Coderow rank ="14" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755951/harden_atkfyy.jpg" codeTitle="James Harden" hoopcodes="518" details="Albania " />
+ 
+<Coderow rank ="15" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755940/herro_bi4pxr.jpg" codeTitle="Tyler Herro" hoopcodes="PO6" details="Rep " />
+<Coderow rank ="16" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755879/tatum_zof8gi.jpg" codeTitle="jayson Tatum" hoopcodes="80P" details="TAFA " />
+<Coderow rank ="17" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610756357/kd_a3nisa.jpg" codeTitle="Kevin Durant" hoopcodes="802" details="Pop back " />
+<Coderow rank ="18" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610926935/lamelonew_vzo7ge.jpg" codeTitle="Lamelo Ball" hoopcodes="804 " details="Pop Reverse" />
+ 
+<Coderow rank ="19" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610757282/currymss_lhtmdk.jpg" codeTitle="Stephen Curry" hoopcodes="318" details="Lousiana " />
+<Coderow rank ="20" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755951/harden_atkfyy.jpg" codeTitle="James Harden" hoopcodes="518" details="Albania " />
+ 
+<Coderow rank ="21" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755940/herro_bi4pxr.jpg" codeTitle="Tyler Herro" hoopcodes="PO6" details="Rep " />
+<Coderow rank ="22" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755879/tatum_zof8gi.jpg" codeTitle="jayson Tatum" hoopcodes="80P" details="TAFA " />
+</div> : null }
+
+<div className="button">
+<button onClick={loadMoreHandler}>Load More</button>
+
+</div>
+ <style jsx>
+     {`
+        a {
+            text-decoration: none;
+      
+          }
+.button {
+    margin-top: 2rem;
+    top: 4px;
+  display: flex;
+    justify-content: center;
+    
+}
+          button {
+            padding: 11px 12px 8px;
+            border-radius: 3px;
+            cursor: pointer;
+            font-family: nyt-franklin, helvetica, arial, sans-serif;
+            transition: all 0.6s ease 0s;
+            white-space: nowrap;
+            vertical-align: middle;
+            background-color: rgb(86, 123, 149);
+            border: 1px solid rgb(50, 104, 145);
+            color: rgb(255, 255, 255);
+            font-size: 11px;
+            line-height: 11px;
+            font-weight: 700;
+            letter-spacing: 0.05em;
+          
+            text-transform: uppercase;
+          }
+     
+     
+     `}
+ </style>
 
     </>
 }
--- pages/index.js
+++ pages/index.js
@@ -3,28 +3,19 @@ import React from 'react';
 import KickerHub from '../components/units/kickerHub'
 import Billboard64 from '../components/units/billboard64'
 import Sectiontitle from '../components/units/sectiontitle'
-import Coderow from '../components/units/coderow'
+import CodeHub from '../components/units/codeHub'
 import Sectionsubtitle from '../components/units/sectionsubtitle'
 function home(){
   return <>
  <main>
-
-   <div id="kickerHub">
+ 
+<div id="kickerHub">
 <KickerHub />
 </div>
 
-<Sectiontitle title="Chart"></Sectiontitle> 
- <a href=""><Coderow rank ="1" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755593/Kenny-Anderson_fci4zv.jpg" codeTitle="Kenny Anders" hoopcodes="232" details="Chibbs " /></a>
-<Coderow rank ="2" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610669340/AIcrossMJ_bmteuq.jpg" codeTitle="Allen Iverson" hoopcodes="013" details="Smoke " />
-<Coderow rank ="3" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610756357/kd_a3nisa.jpg" codeTitle="Kevin Durant" hoopcodes="802" details="Pop back " />
-<Coderow rank ="4" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610926935/lamelonew_vzo7ge.jpg" codeTitle="Lamelo Ball" hoopcodes="804 " details="Pop Reverse" />
- 
-<Coderow rank ="5" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610757282/currymss_lhtmdk.jpg" codeTitle="Stephen Curry" hoopcodes="318" details="Lousiana " />
-<Coderow rank ="6" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755951/harden_atkfyy.jpg" codeTitle="James Harden" hoopcodes="518" details="Albania " />
- 
-<Coderow rank ="7" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755940/herro_bi4pxr.jpg" codeTitle="Tyler Herro" hoopcodes="PO6" details="Rep " />
-<Coderow rank ="8" image="https://res.cloudinary.com/www-hoopscript-com/image/upload/v1610755879/tatum_zof8gi.jpg" codeTitle="jayson Tatum" hoopcodes="80P" details="TAFA " />
- 
+<div id="">
+  <CodeHub />
+</div>
 <Sectiontitle title="Search"/>
   <div id="app">
    <App />
