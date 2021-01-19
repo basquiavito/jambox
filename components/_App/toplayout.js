f224b471c4280bc72cbb084b2db207c13c5ae806
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2021-01-19 16:51:44 -0500

    added pageDrid css

--- components/_App/toplayout.js
+++ components/_App/toplayout.js
@@ -1,161 +1,130 @@
-import { Menu, Container, Image, Icon } from 'semantic-ui-react'
-import Link from 'next/link';
-import Router, { useRouter } from 'next/router'
-import 'semantic-ui-css/semantic.min.css'
+import Link from 'next/link'
  
- 
- 
- 
- 
- 
-Router.onRouteChangeStart = () => NProgress.start()
-Router.onRouteChangeComplete = () => NProgress.done();
-Router.onRouteChangeError = () => NProgress.done();
- 
- 
- 
-function Header() {
- const router = useRouter()
- const user = true;
- 
- function isActive(route) {
-return route === router.pathname;
- }
- return (
-  <Menu stackable fluid id="menu" inverted>
- <Container   text>
- <Link href="/">
-  <Menu.Item header active={isActive('/')}>
-    <Image
-    size="mini"
-    src="/static/logo.svg"
-    style={{marginRight: '1em' }}
-    />
-    HOOPER OS
-  </Menu.Item>
- </Link>
+
+export default function tophead(){
+
+
+
+
+
+    return <>
   
-  
- <Link href="/featured">
- <Menu.Item header active={isActive('/featured')}>
- <Icon
- name="newspaper"
- size="large"
- />
- FEATURED
- </Menu.Item>
- </Link>
-  
- <Link href="/videos">
- <Menu.Item header active={isActive('/videos')}>
- <Icon
- name="play"
- size="large"
- />
- VIDEOS
- </Menu.Item>
- </Link>
- <Link href="/profiles">
-<Menu.Item header active={isActive('/profiles')}>
-<Icon
-name="star"
-size="large"
-/>
-PROFILES
-</Menu.Item>
-</Link>
+<div id="headerLogo">
+
+
+
+<div id="logo">
+ 
+<a  href="/" id="">   
+ <span className="rightSide">HOOPEROS</span> </a>
+ 
+
+</div>
+
+
+
+
+
+
+
+<div id="log">
+ 
+<a  href="/blogs" id="blog">  Blog </a>
+ 
+
+</div>
+   
+
+
  
  
-{user && <Link href="/hoopcodes">
-<Menu.Item header active={isActive('/hoopcodes')}>
-<Icon
-name="code"
-size="large"
-/>
-HOOPCODES
-</Menu.Item>
-</Link>}
- 
-{ user ? (<>
-<Link href="/account">
-<Menu.Item header active={isActive('/account')}>
-<Icon
-name="user"
-size="large"
-/>
-Account
-</Menu.Item>
-</Link>
- 
-<Menu.Item header >
-<Icon
-name="sign out"
-size="large"
-/>
-Logout
-</Menu.Item>
+   
+   
+   </div>
+
+   
  
  
+
  
- 
- 
- 
- 
- 
- 
- 
- 
-<Link href="/cart">
-<Menu.Item header active={isActive('/')}>
-<Icon
-name="cart"
-size="large"
-/>
-Cart
-</Menu.Item>
-</Link>
- 
-</>
- 
- 
- 
- 
- 
- 
- 
- 
-)
-:
-(<>
-<Link href="/login" active={isActive('/login')}>
-<Menu.Item header>
-<Icon
-name="sign in"
-size="large"
-/>
-Login
-</Menu.Item>
-</Link>
- 
- 
-<Link href="/signup">
-<Menu.Item header active={isActive('/')}>
-<Icon
-name="signup"
-size="large"
-/>
-Signup
-</Menu.Item>
-</Link>
- 
- 
- 
- 
-</>)}
-</Container>
- </Menu>)
+<style jsx>
+
+
+
+    {`
+#blog {
+    color: #fff;
+    margin-left: 20px;
 }
+
+
+  #logo{
+    margin-right: 10px;
+    color: #fff;
+    letter-spacing: -4;
+}
+
+
+
+.rightSide {
+  color:  #fff;
+  letter-spacing: -4;
+}
+#hoopers {
+     cursor: pointer;
+    display: block;
+    height: 2.825rem;
+    width: 2.825rem;
+    position: relative;
+    border: 0;
+    border-radius: 50%;
+    outline: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+    user-select: none;
+    z-index: 1069;
+    text-align: center;
+    padding-top: 10px;
+}
+svg {
+  
+    fill: #212121;
+    width: 1.5rem; 
+  
+     
+ }
  
-export default Header;
+#headerLogo {
+    display: flex;
+    transition: opacity .3s ease-in-out;
+    justify-content: space-between;
+    height: 41px;
+ background-color:#051c2d;
+    width: 100%;
+    height: 41px;
+   color:  rgb(255 255 255); 
+    justify-content: space-between;
+    align-items: center;
+    padding-left: 8px;
+    padding-right: 8px;
+} 
+a {
+    color:#212121;
+    text-decoration: none;
+}
+   h1 {
+      font-size: 22px;
+      
+  }
+  #logo {
+      font-size: 28px;
+      
+      font-weight: bold;
+  }
+`}
+</style>
+    </>
 
 
+} 
